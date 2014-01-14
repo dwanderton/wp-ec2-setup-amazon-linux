cad798ecb09dcb8e7c3d07b227ae3950f9b3cb7f
dwanderton <dwanderton@gmail.com>
2014-01-14 18:21:31 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Simple setup.sh for configuring Amazon Linux AMI 2013 instance
+# for headless setup. 
+
+# Install basic tools
+sudo yum install -y git
+sudo yum install -y curl
+
+# git pull and install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+
+git clone https://github.com/dwanderton/dotfiles.git
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+
+sudo yum install -y httpd
+sudo yum install php php-mysql php-xml
+sudo yum install mysql-server
+sudo mysqladmin -uroot create maindb
+
+cd /var/www/html
+wget http://wordpress.org/latest.tar.gz
+sudo tar -xzvf latest.tar.gz
+cd wordpress
+sudo mv wp-config-sample.php wp-config.php
+sudo chown -R  apache /var/www/html
+sudo service httpd start
+mysql_secure_installation
+
+
+sudo vi wp-config.php
+sudo service httpd restart
